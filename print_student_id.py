598108ab33c64b70d7566c938d9c15ef629f3d2e
Pepperkanyaluck <124992580+Pepperkanyaluck@users.noreply.github.com>
2023-02-10 15:05:42 +0700

    Add calculate sum function

--- print_student_id.py
+++ print_student_id.py
@@ -4,7 +4,8 @@ def print_id(student_id):
     print("Student ID = ", student_id)
 
 def calculate_sum(array):
-    return array
+    return array.sum()
+    
 if __name__ == "__main__":
         print_id("34567")
         
